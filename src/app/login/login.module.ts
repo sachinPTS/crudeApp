5c4b7f0d052236b8898a59efbd0474a428622344
sachinPTS <sachin@pureecosoft.com>
2022-01-31 09:05:41 +0530

    child routing for login module

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { ForgotpassComponent } from './components/forgotpass/forgotpass.component';
 
+const routes: Routes = [
+{path:"login",component:LoginComponent},
+{path:"registration",component:RegistrationComponent},
+{path:"forgotPassword",component:ForgotpassComponent}
 
+];
 
 @NgModule({
   declarations: [LoginComponent, RegistrationComponent, ForgotpassComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class LoginModule { }
